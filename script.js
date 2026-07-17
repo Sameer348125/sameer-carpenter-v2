658cc21b9b83d7461e6c91272108ea44e30f2500
Sameer348125 <karpenterdesign99@gmail.com>
2026-07-17 20:27:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -34,3 +34,35 @@ window.open(this.src,"_blank");
 };
 
 });
+// ===== Lightbox =====
+
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const closeBtn = document.getElementById("close");
+
+document.querySelectorAll(".gallery-box img").forEach(function(img){
+
+    img.addEventListener("click", function(){
+
+        lightbox.style.display = "flex";
+        lightboxImg.src = this.src;
+
+    });
+
+});
+
+closeBtn.addEventListener("click", function(){
+
+    lightbox.style.display = "none";
+
+});
+
+lightbox.addEventListener("click", function(e){
+
+    if(e.target === lightbox){
+
+        lightbox.style.display = "none";
+
+    }
+
+});
