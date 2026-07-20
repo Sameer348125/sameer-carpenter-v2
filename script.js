a7efe0107f0e0b4e43f81e3cb2121a979ce08540
Sameer348125 <karpenterdesign99@gmail.com>
2026-07-21 00:25:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -42,4 +42,50 @@ if (heroSlide) {
 
     }, 3000);
 
-}
+}// =========================
+// GALLERY DATA
+// =========================
+
+const galleryData = {
+
+    "TV Panel": [
+        "image/tv-panel-1.jpg",
+        "image/tv-panel-2.jpg",
+        "image/tv-panel-3.jpg",
+        "image/tv-panel-4.jpg"
+    ],
+
+    "Modular Kitchen": [
+        "image/download.webp",
+        "image/download (1).webp"
+    ],
+
+    "Wardrobe": [
+        "image/images (12).jpeg",
+        "image/images (19).jpeg"
+    ],
+
+    "Wooden Door": [
+        "image/images (10).jpeg",
+        "image/images (11).jpeg"
+    ],
+
+    "Wooden Furniture": [
+        "image/modern-furniture-1.jpg",
+        "image/modern-furniture-2.jpg"
+    ]
+
+};
+
+// =========================
+// GALLERY VARIABLES
+// =========================
+
+const popup = document.getElementById("galleryPopup");
+const popupImg = document.getElementById("galleryImage");
+const closeGallery = document.getElementById("closeGallery");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+let currentImages = [];
+let currentIndex = 0;
